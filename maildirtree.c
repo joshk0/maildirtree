0cc468e6442c4bb5700c3d67bbd352750cbe3b09
Joshua Kwan <joshk@triplehelix.org>
2003-07-08 04:32:24 +0000

    still no worky
    git-svn-id: file:///svn/maildirtree/trunk@4 6917002c-0bd3-0310-b9ff-c23b3cd7da21

--- maildirtree.c
+++ maildirtree.c
@@ -14,6 +14,8 @@
  * GNU General Public License for more details.
  */
 
+#define _GNU_SOURCE /* strdup in C99 */
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -24,101 +26,144 @@
 #include <errno.h>
 
 struct Directory {
-	char* name;
-	struct Directory ** subdirs;
-	char** contents;
+  char* name = NULL;
+  struct Directory ** subdirs = NULL;
+  int last_subdir_index = 0;
 };
+
+#if __STDC_VERSION__ < 199901L
+typedef enum { false = 0, true } bool;
+#endif
 
 static const char indent [] = "   ";
 static unsigned int indentlen = 3;
 
+static bool find_subdirs (struct Directory * subdir, char* token);
 static struct Directory * hier_sort (char** dirs);
 static char** read_this_dir (DIR* d);
 static void print_tree (struct Directory * start, unsigned int level);
 
 int main (int argc, char* argv[])
 {
-	DIR * maildir;
-	struct Directory * root;
-	char ** dirs;
-	
-	if (argc >= 2) {
-		if ((maildir = opendir(argv[1])) != NULL) {
-			dirs = read_this_dir(maildir);
+  DIR * maildir;
+  struct Directory * root;
+  char ** dirs;
+  
+  if (argc >= 2) {
+    if ((maildir = opendir(argv[1])) != NULL) {
+      dirs = read_this_dir(maildir);
 
-			/* debug */
-			while (dirs != NULL && *dirs != NULL) {
-				puts (*dirs);
-				dirs++;
-			}
-			
-			closedir(maildir);
+      /* debug */
+      while (dirs != NULL && *dirs != NULL) {
+        puts (*dirs);
+        dirs++;
+      }
+      
+      closedir(maildir);
 
-			root = hier_sort(dirs);
-			
-			print_tree (root, 0);
-			
-		}
-		else {
-			printf ("%s: %s: %s\n", argv[0], argv[1],
-					strerror(errno));
-			exit (1);
-		}
-	}
-	else {
-		printf ("%s: must have a directory argument\n", argv[0]);
-		exit (1);
-	}
+      root = hier_sort(dirs);
+      
+      print_tree (root, 0);
+      
+    }
+    else {
+      printf ("%s: %s: %s\n", argv[0], argv[1],
+        strerror(errno));
+      exit (1);
+    }
+  }
+  else {
+    printf ("%s: must have a directory argument\n", argv[0]);
+    exit (1);
+  }
 
-	return 0;
+  return 0;
 }
 
 static char** read_this_dir (DIR* d)
 {
-	struct dirent *entries;
-	char ** result = NULL;
-	int count = 0;
-	
-	while ((entries = readdir(d)) != NULL) {
-		if (*entries->d_name == '.' &&
-		    strcmp(entries->d_name, ".") &&
-		    strcmp(entries->d_name, ".."))
-		{
-			result = realloc (result, (++count)*sizeof(char**));
-			result[count-1] = malloc (entries->d_reclen + 1);
-			result[count-1] = strdup (entries->d_name);
-		}
-	}
+  struct dirent *entries;
+  char ** result = NULL;
+  int count = 0;
+  
+  while ((entries = readdir(d)) != NULL) {
+    if (*entries->d_name == '.' &&
+        strcmp(entries->d_name, ".") &&
+        strcmp(entries->d_name, ".."))
+    {
+      result = realloc (result, (++count)*sizeof(char**));
+      result[count-1] = malloc (entries->d_reclen + 1);
+      /* remove the leading dot; not used later */
+      result[count-1] = strdup ((entries->d_name)+1);
+    }
+  }
 
-	return result;
+  return result;
 }
 
+/* hier_sort()
+ * 
+ * precondition: dirs points to a listing of directories in a Maildir
+ * as created by read_this_dir
+ * 
+ * side effects: dirs will be WASTED, don't use it again; deep copy it
+ * if necessary?
+ */
 static struct Directory * hier_sort (char** dirs)
 {
-	struct Directory *dir = malloc(sizeof(struct Directory));
+  struct Directory *dir = malloc(sizeof(struct Directory));
+  struct Directory *sub = NULL;
+  dir->subdirs = NULL;
+  
+  char *tokens;
 
-	
-	
-	return dir;
+  while (*dirs != NULL)
+  {
+    tokens = strtok(*dirs, "."); /* This must not be null due to read_this_dir
+				  * filtering */
+    if (dir->subdirs != NULL && (sub = find_subdirs(dir->subdirs, tokens)) != NULL) // XXX O(n)
+    {
+      
+    }
+    else /* create the subdir */
+    {
+      dir->subdirs[++last_subdir_index] = malloc(sizeof(struct Directory));
+      dir->subdirs[last_subdir_index]->name = tokens;
+    }
+    /* DURR */
+  }
+  
+  return dir;
+}
+
+static struct Directory * find_subdirs (struct Directory * subdir, char* token)
+{
+  int i;
+  assert(subdir != NULL);
+
+  for (i = 0; i < num_subdirs; i++)
+  {
+    assert(subdir[i] != NULL);
+    if (!strcmp(subdir[i]->name, token))
+      return subdir[i];
+  }
+  return NULL;
 }
 
 static void print_tree (struct Directory * start, unsigned int level)
 {
-	char* in = malloc ((level * indentlen) + 3);
-	unsigned char bar;
-	unsigned int i;
-	
-	for (i = 0; i <= level; i++)
-		strcat (in, indent);
-	
-	while (start->subdirs != NULL) {
-		bar = ((start->subdirs)+1 == NULL) ? '`' : '|';
-		printf("%s%c %s\n", indent, bar, (*(start->subdirs))->name);
-		print_tree (*(start->subdirs++), level + 1);
-	}
-	
-	while (start->contents != NULL) {
-		bar = ((start->contents)+1 == NULL) ? '`' : '|';
-		printf ("%s%c %s\n", indent, bar, *(start->contents++));
-	}
+  char* in = (level == 0) ? "" : malloc ((level * indentlen) + 1);
+  unsigned char bar;
+  unsigned int i;
+  
+  for (i = 0; i <= level; i++)
+    strcat (in, indent);
+  
+  printf ("%s%c %s\n", in, start->name);
+  
+  while (start->subdirs != NULL) {
+    bar = ((start->subdirs)+1 == NULL) ? '`' : '|';
+    printf("%s%c %s\n", in, bar, (*(start->subdirs))->name);
+    print_tree (*(start->subdirs++), level + 1);
+  }
 }
